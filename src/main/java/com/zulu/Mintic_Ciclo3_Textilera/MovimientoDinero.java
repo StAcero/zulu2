0fd938c93d681306fd2934079f0d7a3b7c8aeab9
Yohan-Rodriguez <ymrodriguezg@upn.edu.co>
2022-08-26 15:15:54 -0500

    Primer commit branch yohan

--- src/main/java/com/zulu/Mintic_Ciclo3_Textilera/MovimientoDinero.java
+++ src/main/java/com/zulu/Mintic_Ciclo3_Textilera/MovimientoDinero.java
@@ -1,4 +1,37 @@
 package com.zulu.Mintic_Ciclo3_Textilera;
 
 public class MovimientoDinero {
+    float montoDinero = 0.0f;       // Montos positivos y negativos de x movimiento.
+    String conceptoMovimiento;      // Descripción del movimiento.
+    String idUser;      //Id del perfil del ususario que realizo del movimiento del dinero
+
+    public MovimientoDinero (float montoDinero, String conceptoMovimiento, String idUser){
+        this.montoDinero = montoDinero;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.idUser = idUser;
+    }
+
+    public float getMontoDinero() {
+        return montoDinero;
+    }
+
+    public void setMontoDinero(float montoDinero) {
+        this.montoDinero = montoDinero;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
 }
