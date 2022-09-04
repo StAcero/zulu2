8a2f901e069b1d6891ab10635649669f330db9ed
Felipe Granda R <felipegranda@hotmail.com>
2022-09-03 23:58:57 -0500

    ControladorUsuarios, Empleadorepo, EmpleadoServicio creados. Base de datos conectada con supabase, tablas de Empleado y empresa creadas. ruta /users GET Y POSTretorna 200, ruta /users/{id} GET, PATCH Y DELETE retornan 200. El controlador ya es funcional, devuelve, agrega y elimina información.

---
+++ src/main/java/com/zulu/Mintic_Ciclo3_Textilera/entities/NombresDeRol.java
@@ -0,0 +1,18 @@
+package com.zulu.Mintic_Ciclo3_Textilera.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.stream.Stream;
+
+
+public enum NombresDeRol {
+
+    ADMINISTRADOR,
+    OPERATIVO,
+
+
+
+
+
+}
+
